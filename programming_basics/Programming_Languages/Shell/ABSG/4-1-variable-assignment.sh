9cd6d84346afab72cee637dcaf924b98af54bd4a
chaoshen <45914118+wxx26caca@users.noreply.github.com>
2022-10-17 14:53:09 +0800

    example 4-1
    Variable assignment and substitution.

---
+++ programming_basics/Programming_Languages/Shell/ABSG/4-1-variable-assignment.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Variables: assignment and substitution
+
+a=375
+hello=$a
+
+#-------------------------------------------------------------------------
+# No space permitted on either side of = sign when initializing variables.
+# What happens if there is a space?
+
+#  "VARIABLE =value"
+#           ^
+#% Script tries to run "VARIABLE" command with one argument, "=value".
+
+#  "VARIABLE= value"
+#            ^
+#% Script tries to run "value" command with
+#+ the environmental variable "VARIABLE" set to "".
+#-------------------------------------------------------------------------
+
+echo hello       # output: hello, but it just the string "hello" ...
+echo $hello      # output: 375, a variable reference.
+echo ${hello}    # output: 375, a variable reference, as above.
+echo "$hello"    # output: 375, quoting.
+echo "${hello}"  # output: 375, quoting.
+
+echo
+
+hello="A B  C   D"
+echo $hello     # A B C D
+echo "$hello"   # A B  C   D
+# ----------------------------------------
+# Quoting a variable preserves whitespace.
+# ----------------------------------------
+
+echo
+
+echo '$hello'  # $hello
+#  Variable referencing disabled (escaped) by single quotes,
+#+ which causes the "$" to be interpreted literally.
+
+hello=    # Setting it to a null value.
+echo "\$hello (null value) = $hello"      # $hello (null value) =
+#  Setting a variable to a null value is not the same as unsetting it
+#+ although the end result is the same (see below).
+
+var1=21  var2=22  var3=$V3
+echo
+echo "var1=$var1   var2=$var2   var3=$var3"
+
+# --------------------------------------------------------------
+#  It is permissible to set multiple variables on the same line,
+#+ if separated by white space.
+#  Caution, this may reduce legibility, and may not be portable.
+#  May cause problems with legacy versions of "sh" . . .
+# --------------------------------------------------------------
+
+echo; echo
+
+numbers="one two three"
+other_numbers="1 2 3"
+#  If there is whitespace embedded within a variable,
+#+ then quotes are necessary.
+#  other_numbers=1 2 3                  # Gives an error message.
+echo "numbers = $numbers"
+echo "other_numbers = $other_numbers"   # other_numbers = 1 2 3
+
+#  Escaping the whitespace also works.
+mixed_bag=2\ ---\ Whatever             # Space after escape(\).
+echo "$mixed_bag"         # 2 --- Whatever
+
+echo; echo
+
+echo "uninitialized_variable = $uninitialized_variable"
+# output: uninitialized_variable =
+uninitialized_variable=   #  Declaring, but not initializing it.
+echo "uninitialized_variable = $uninitialized_variable"
+                          # It still has a null value.
+uninitialized_variable=23       # Set it.
+unset uninitialized_variable    # Unset it.
+echo "uninitialized_variable = $uninitialized_variable"
+                                # uninitialized_variable =
+                                # It still has a null value.
+echo
+
+exit 0
